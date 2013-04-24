eec3c59a6385a866678157a8e469c698ca828f81
capnm <capnm@capitanio.org>
2013-04-25 00:10:30 +0200

    add *link udev rules

--- etc-rules-udev.d/install.sh
+++ etc-rules-udev.d/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-f="ti_launchpad.rules"
+f="launchpad_link.rules"
 cp -v $f /tmp/
 sudo cp -v "/tmp/$f" "/etc/udev/rules.d/"
 rm -f "/tmp/$f"
@@ -7,4 +7,4 @@ rm -f "/tmp/$f"
 #sudo stop udev
 #sudo start udev
 
-echo "done, re-plug your LaunchPad"
+echo "done, re-plug your Link or LaunchPad"
